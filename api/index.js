de3d4e4569fdfc59ecb751a6adab25341585ab26
Colby McHenry <me@colbymchenry.com>
2023-07-18 12:06:45 -0500

    Production ready

--- api/index.js
+++ api/index.js
@@ -16,20 +16,22 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
-    Query: {
-        hello: () => "world",
-    },
+  Query: {
+    hello: () => "world",
+  },
 };
 
 const startApolloServer = async (app, httpServer) => {
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    });
+  const server = new ApolloServer({
+    playground: true,
+    introspection: true,
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-    await server.start();
-    server.applyMiddleware({ app });
+  await server.start();
+  server.applyMiddleware({ app });
 }
 
 startApolloServer(app, httpServer);
