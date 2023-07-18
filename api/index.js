746fdefdec3a4d388c9b23c4cc59de49b0836309
Colby McHenry <me@colbymchenry.com>
2023-07-18 12:35:21 -0500

    Moved firebaseadmin to local api

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import http from "http";
 import express from "express";
 import cors from "cors";
 import {config} from "dotenv";
-import { FirebaseAdmin } from "./FirebaseServer";
+import admin from 'firebase-admin';
+
 
 config();
 
@@ -89,3 +90,48 @@ startApolloServer(app, httpServer);
 
 
 export default httpServer;
+
+
+
+try {
+  admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(process.env.NEXT_FIREBASE_ADMIN)),
+  })
+
+  admin.firestore().settings({
+      timestampsInSnapshots: true,
+      ignoreUndefinedProperties: true
+  })
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test(error.message)) {
+      console.error('Firebase admin initialization error', error.stack)
+  }
+}
+class FirebaseAdmin {
+
+  static firestore() {
+      return admin.firestore();
+  }
+
+  static serverTimestamp() {
+      return admin.firestore.FieldValue.serverTimestamp();
+  }
+
+  static auth() {
+      return admin.auth();
+  }
+
+  static async getCollectionArray(collection) {
+      const querySnapshot = await FirebaseAdmin.firestore().collection(collection).get();
+      let result = []
+      querySnapshot.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id })
+      });
+      return result;
+  }
+
+}
