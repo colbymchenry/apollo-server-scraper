7bc5d09e349a8a19a6fafd7770906e58554b1257
Colby McHenry <me@colbymchenry.com>
2023-07-19 13:26:41 -0500

    Update schema

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import http from "http";
 import express from "express";
 import cors from "cors";
 import helmet from 'helmet';
-import {config} from "dotenv";
+import { config } from "dotenv";
 import admin from 'firebase-admin';
 
 
@@ -67,7 +67,7 @@ const resolvers = {
       console.log("aRGS", args)
       console.log("ID: ========> " + args.id)
       const doc = await FirebaseAdmin.firestore().collection("scrapers").doc(id).get();
-      return {id: doc.id, ...doc.data()}
+      return { id: doc.id, ...doc.data() }
     }
   },
 };
@@ -78,20 +78,17 @@ const startApolloServer = async (app, httpServer) => {
     introspection: true,
     typeDefs,
     resolvers,
+    engine: {
+      reportSchema: true,
+    },
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
   await server.start();
   server.applyMiddleware({ app });
-
-  setTimeout(function () {
-    server.schema = buildSchema()
-  }, 10000)
 }
 
 startApolloServer(app, httpServer);
-
-
 
 export default httpServer;
 
@@ -99,12 +96,12 @@ export default httpServer;
 
 try {
   admin.initializeApp({
-      credential: admin.credential.cert(JSON.parse(process.env.NEXT_FIREBASE_ADMIN)),
+    credential: admin.credential.cert(JSON.parse(process.env.NEXT_FIREBASE_ADMIN)),
   })
 
   admin.firestore().settings({
-      timestampsInSnapshots: true,
-      ignoreUndefinedProperties: true
+    timestampsInSnapshots: true,
+    ignoreUndefinedProperties: true
   })
 } catch (error) {
   /*
@@ -112,30 +109,30 @@ try {
    * not an actual error when we're hot-reloading.
    */
   if (!/already exists/u.test(error.message)) {
-      console.error('Firebase admin initialization error', error.stack)
+    console.error('Firebase admin initialization error', error.stack)
   }
 }
 class FirebaseAdmin {
 
   static firestore() {
-      return admin.firestore();
+    return admin.firestore();
   }
 
   static serverTimestamp() {
-      return admin.firestore.FieldValue.serverTimestamp();
+    return admin.firestore.FieldValue.serverTimestamp();
   }
 
   static auth() {
-      return admin.auth();
+    return admin.auth();
   }
 
   static async getCollectionArray(collection) {
-      const querySnapshot = await FirebaseAdmin.firestore().collection(collection).get();
-      let result = []
-      querySnapshot.forEach((doc) => {
-          result.push({ ...doc.data(), id: doc.id })
-      });
-      return result;
+    const querySnapshot = await FirebaseAdmin.firestore().collection(collection).get();
+    let result = []
+    querySnapshot.forEach((doc) => {
+      result.push({ ...doc.data(), id: doc.id })
+    });
+    return result;
   }
 
 }
